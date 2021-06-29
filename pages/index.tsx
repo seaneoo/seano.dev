4170a6d1e7ce724b4a68d71a79c355b60c8f20aa
Sean O'Connor <seanoconnor941@gmail.com>
2021-06-28 20:54:16 -0600

    Add favicon-related tags, images, and manifest

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,10 @@ export default function Home() {
         <meta httpEquiv='X-UA-Compatible' content='ie=edge' />
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
         <title>Sean O&apos;Connor</title>
+        <link rel='shortcut icon' href='/favicon.ico' type='image/x-icon' />
+        <link rel='icon' href='/icon.svg' type='image/svg+xml' />
+        <link rel='apple-touch-icon' href='/apple-touch-icon.png' />
+        <link rel='manifest' href='/manifest.webmanifest' />
       </Head>
     </React.Fragment>
   );
