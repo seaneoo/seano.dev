3c31aa77095f08014bc97b5061c31da1b8dfcf9c
Sean O'Connor <seanoconnor941@gmail.com>
2021-06-28 21:38:48 -0600

    Removed jsxSingleQuote

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,13 +5,13 @@ export default function Home() {
   return (
     <React.Fragment>
       <Head>
-        <meta httpEquiv='X-UA-Compatible' content='ie=edge' />
-        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+        <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Sean O&apos;Connor</title>
-        <link rel='shortcut icon' href='/favicon.ico' type='image/x-icon' />
-        <link rel='icon' href='/icon.svg' type='image/svg+xml' />
-        <link rel='apple-touch-icon' href='/apple-touch-icon.png' />
-        <link rel='manifest' href='/manifest.webmanifest' />
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/manifest.webmanifest" />
       </Head>
     </React.Fragment>
   );
